777ca9e8fc333276b31ee5d62958b1b293aca6a6
Folashade Ayorinde-Akinola <folashadeayorindeakinola@gmail.com>
2024-07-03 10:05:38 +0100

    Funfact

---
+++ Funfact.py
@@ -0,0 +1,6 @@
+#Share some fun facts you know about the JavaScript programming language. 
+# Follow the link below to get started.https://javascript.plainenglish.io/javascript-fun-facts-to-share-at-parties-9721527591de
+
+Print("JavaScript was probably named after Java")
+
+print("Executed successful!")
