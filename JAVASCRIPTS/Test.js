930d30f6ced0a431b6a8caa8dd16bc312951425b
Folashade Ayorinde-Akinola <folashadeayorindeakinola@gmail.com>
2024-07-09 10:16:33 +0100

    file update

--- JAVASCRIPTS/Test.js
+++
@@ -1,7 +0,0 @@
-const myFavouriteLanguage = "Javascripts"
-const age = 20
-const name = "Folashade"
-const sentence = 'My name is ' + name + ',' + 'I am ' + age + 'years old'
-
-
-console.log(sentence)
--- app.js
+++
@@ -1,2 +0,0 @@
-//To write a JavaScript program that shows the text, Hello, world! As an alert on the browser.
-alert('Hello world!')
