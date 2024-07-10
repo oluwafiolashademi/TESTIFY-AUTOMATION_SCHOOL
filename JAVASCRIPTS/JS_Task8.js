49eaf8a705edf7613aff863e2f1a8c418196a4db
Folashade Ayorinde-Akinola <folashadeayorindeakinola@gmail.com>
2024-07-10 09:15:19 +0100

    Odd Numbers

---
+++ JAVASCRIPTS/JS_Task8.js
@@ -0,0 +1,7 @@
+// Odd numbers are NOT divisible by 2. 
+
+// Write a Javascript program that prints out all the odd numbers between 1 and 20. Your code must use a for-loop.
+
+for(let number = 1; number <= 20; number = number+2){
+    console.log(number + ' is an odd number') 
+    }
