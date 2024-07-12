c96564558da543c9a83d3e6a4c705e803b94fab6
Folashade Ayorinde-Akinola <folashadeayorindeakinola@gmail.com>
2024-07-12 16:51:08 +0100

    Best friend

---
+++ JAVASCRIPTS/JS_Task12.js
@@ -0,0 +1,16 @@
+// Task: Pick your Best Friend
+// Elements of an array are index based. 
+
+// From your array of best created in the previous task, choose your best friend based on their index in the array. 
+
+// Steps: 
+
+// On the next line below your array of friends, Declare a variable, bestFriend and assign your best friend from the array of friends. (Use bracket notation)
+
+// Log bestFriend to the console to ensure you chose the right friend.
+
+const friendArray = ['Folashade', 'Daniel', 'Gideon', 'Oreofe', 'Toyyib']
+
+const bestFriend = friendArray[3]
+
+console.log(bestFriend)
