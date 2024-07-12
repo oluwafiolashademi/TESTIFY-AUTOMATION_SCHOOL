bad39d86bece76142734ffdc45d3a1202174fac3
Folashade Ayorinde-Akinola <folashadeayorindeakinola@gmail.com>
2024-07-12 16:13:02 +0100

    Array of friends

---
+++ JAVASCRIPTS/JS_Task11.js
@@ -0,0 +1,15 @@
+// A JavaScript array is basically an ordered list of comma-separated values. Each value is called an element.
+
+// Create a JavaScript array for top 5 friends.
+
+// Steps: 
+
+// Declare a variable, friends and assign it an array [ ]
+
+// Add 5 elements in the array (Each element should be of type string. Remember to separate each element by a comma.)
+
+// Log your friends array to the console and take a look.
+
+const friendsArray = ['Folashade', 'Daniel', 'Gideon', 'Oreofe', 'Toyyib']
+
+console.log(friendsArray)
