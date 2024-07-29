4fcf499e34f206ae949f366ae66afaf260434188
Folashade Ayorinde-Akinola <folashadeayorindeakinola@gmail.com>
2024-07-29 21:59:28 +0100

    Renaming of Task for task 2 and 4

--- MODULE 3/3A_JAVASCRIPTS/JAVASCRIPTS CODING CHALLENGE/Task10.js
+++ MODULE 3/3A_JAVASCRIPTS/JAVASCRIPTS CODING CHALLENGE/Task10.js
@@ -2,14 +2,24 @@
 
 // 10. Create a function that filters out negative numbers.
 
-function filters(numbers){
-    for (let num = 0; num < number.length; num++) {
-        if (numbers[num] < 0){
-            console.log((numbers[num]) + " is a negative number")
-        }
-        else{
-            console.log((numbers[num]) + " is a positive number")
-        }
-    }
-}
-filters([-5, 0, 7, -2, 10]);
+// function filters(numbers){
+//     for (let num = 0; num < number.length; num++) {
+//         if (numbers[num] < 0){
+//             console.log(('${numbers[num]} is a negative number'))
+//         }
+//         else{
+//             console.log(('${numbers[num]} is a positive number'))
+//         }
+//     }
+// }
+// filters([-5, 0, 7, -2, 10]);
+
+
+function filterPositives(numbers) {
+    const positiveNumbers = numbers.filter(num => num >= 0);
+    return positiveNumbers;
+  }
+  
+  const numbers = [-5, 0, 7, -2, 10];
+  const filteredNumbers = filterPositives(numbers);
+  console.log(filteredNumbers); // Output: [0, 7, 10]
