6d8de217f3ef9329cd985658b4e947d4203426c6
Folashade Ayorinde-Akinola <folashadeayorindeakinola@gmail.com>
2024-09-02 18:01:47 +0100

    Module 4A Task56

---
+++ MODULE 4/TASK/Task6.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class Task6 {
+    public static void main(String[] args) {
+        String word = "DEMOCRACY";
+        String come = word.substring(1,5);
+        System.out.println(come);
+        String reversed = new StringBuilder(come).reverse().toString();
+        System.out.println(reversed);
+    }
+}
