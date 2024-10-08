df07c9a28b6a6d421ee3a0d94476d41b0c7eac3a
Folashade Ayorinde-Akinola <folashadeayorindeakinola@gmail.com>
2024-10-08 10:59:48 +0100

    Module 4B Task10

--- MODULE 4B/src/main/java/Task10.java
+++ MODULE 4B/src/main/java/Task10.java
@@ -2,10 +2,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class Task10 {
     //Navigate to https://jqueryui.com/
