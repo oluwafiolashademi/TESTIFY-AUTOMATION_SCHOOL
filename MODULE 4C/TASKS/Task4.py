6d2f4c087ae9d0d2fbe37d1d3b45c51e7167a452
Folashade Ayorinde-Akinola <folashadeayorindeakinola@gmail.com>
2024-11-01 16:29:35 +0100

    MODULE 4C Task4

---
+++ MODULE 4C/TASKS/Task4.py
@@ -0,0 +1,15 @@
+"""
+Declare two numbers with any values
+Add or Subtract or Multiply the two numbers together
+Print out the value of the operation
+"""
+x = 10
+y = 6
+result = x + y
+print(result)
+
+result = x - y
+print(result)
+
+result = x * y
+print(result)
