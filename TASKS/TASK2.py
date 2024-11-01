2aa6b7845ff540df4af84efd9a65f1f5667a31c8
Folashade Ayorinde-Akinola <folashadeayorindeakinola@gmail.com>
2024-11-01 12:27:54 +0100

    MODULE 4C TASK2

---
+++ TASKS/TASK2.py
@@ -0,0 +1,9 @@
+"""
+Declare a variable with variable_name as my_var and value as 12.6
+Declare an integer variable with the value of 200
+Declare a String variable with the value of your name
+"""
+
+my_var = 12.6
+number = 200
+name = "Folashade"
