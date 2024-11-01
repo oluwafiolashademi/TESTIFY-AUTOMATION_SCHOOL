bc6113d2c92472b6b3ba12d11a2a904948f88e3e
Folashade Ayorinde-Akinola <folashadeayorindeakinola@gmail.com>
2024-11-01 15:08:40 +0100

    MODULE 4C Task3

---
+++ MODULE 4C/TASKS/Task3.py
@@ -0,0 +1,12 @@
+"""
+Declare a raw string
+
+Declare two strings with any value
+Concatenate the two string
+Print out the value of the concatenated strings
+"""
+
+name = "My Name is Folashade, "
+value = "I'm currently learning Automation Testing at Testify"
+sentences = (name + value)
+print(sentences)
