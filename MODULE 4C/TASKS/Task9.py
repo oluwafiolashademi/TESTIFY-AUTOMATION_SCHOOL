a44090a2c742b5880798bd1e197ca5cb3fccd6c6
Folashade Ayorinde-Akinola <folashadeayorindeakinola@gmail.com>
2024-11-07 09:36:01 +0100

    MODULE 4C Task9

---
+++ MODULE 4C/TASKS/Task9.py
@@ -0,0 +1,17 @@
+"""
+Create a function that prints out "Hello World"
+Create a function that does nothing, an empty function
+Invoke/call both functions
+"""
+
+
+def greet():
+    print("Hello World")
+
+
+def empty():
+    pass
+
+
+greet()
+empty()
